d7ef43dace6c27dc144032f25bdcd548e3d73c50
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-17 12:47:50 -0800

    Fix type-o in comments

--- Basic Assignments/ShippingQuote/ShippingQuote/Program.cs
+++ Basic Assignments/ShippingQuote/ShippingQuote/Program.cs
@@ -13,7 +13,7 @@ namespace ShippingQuote
             Console.WriteLine("Please enter the package weight in pounds:");
             double weight = Convert.ToDouble(Console.ReadLine());
             
-            // If package weighs more than 50 it's too heavy else continue with the dimensions
+            // If package weighs more than 50 it's too heavy else continue with the user input of package dimensions
             if (weight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
@@ -37,7 +37,7 @@ namespace ShippingQuote
                 // Convert to decimal when dealing with money quotes
                 decimal quote = Convert.ToDecimal(((width * height * length) * weight) / 100);
 
-                // Ternary Operator: If package dimesnion is over 50 it's too be else provide quote
+                // Ternary Operator: If package dimension is over 50 it's too big else provide quote
                 string result = dimension > 50 ? "\nPackage too big to be shipped via Package Express." : "\nYour estimated total for shipping this package is: $" + quote + "\nThank you!";
                 Console.WriteLine(result);
 
