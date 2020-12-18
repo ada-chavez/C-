1e59b829f7d2856295dbc17d14f1537d7209716d
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-18 09:31:35 -0800

    initial commit

---
+++ Basic Assignments/GoldilocksApp/GoldilocksApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace GoldilocksApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How does Goldilocks like her porridge: cold, hot, or warm?");
+            string porridgeTemp = Console.ReadLine();
+            
+            // Set guess to True when porridgeTemp is "warm"
+            bool guess = porridgeTemp == "warm";
+
+            // Do while statment. Do this while porridgeTemp is not "warm"
+            do
+            {
+                switch (porridgeTemp)
+                {
+                    case "cold":
+                        Console.WriteLine("Ew she doesn't want cold gummy porridge.\nTry again!");
+                        porridgeTemp = Console.ReadLine();
+                        break;
+
+                    case "hot":
+                        Console.WriteLine("Oh this porridge is much too hot! She may burn her tongue.\nTry again!");
+                        porridgeTemp = Console.ReadLine();
+                        break;
+
+                    case "warm":
+                        Console.WriteLine("Oh warm porridge is just right! Now maybe there's a bed she can find to nap in...");                                           
+                        break;
+
+                    default:
+                        Console.WriteLine("Oops! Please enter one of the porridge choices: \n\"cold\" \"hot\" or \"warm\"");
+                        porridgeTemp = Console.ReadLine();
+                        break;
+
+
+                }
+            }
+            while (!guess);
+            Console.Read();
+        }
+    }
+}
