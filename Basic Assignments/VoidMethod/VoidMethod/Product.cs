fd2e6a4f7d351cf439b821cc32fb999a6274572d
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-22 13:16:14 -0800

    initial commit

---
+++ Basic Assignments/VoidMethod/VoidMethod/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Product
+    {
+        // void specifies that this method doesn't return a value
+        public void Multiply (int num1, out int num2)
+        {
+            // Math operation on first parameter behind the scenes
+            int result = num1 * 2;
+
+            // Display second parameter to screen
+            num2 = 33;
+            Console.WriteLine("The second parameter value is: {0} ", num2);
+        }
+    }
+}
---
+++ Basic Assignments/VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Product class
+            Product product = new Product();
+
+            // Call the  Multiply Method passing parameters by name
+            product.Multiply(num1: 10, num2: out int num2);
+
+            Console.ReadLine();
+        }
+    }
+}
