9ea617fa0dc31804d594bd1a8794169cb03c7985
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-22 12:43:38 -0800

    initial commit

---
+++ Basic Assignments/OptionalParameter/OptionalParameter/Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    public class Add
+    {
+        // mehod with two parameters, one parameter optional
+        public int Sum (int num1, int num2 = 0)
+        {
+            int sum = num1 + num2 + 5;
+            return sum;
+        }
+    }
+}
---
+++ Basic Assignments/OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating Add Class
+            Add add = new Add();
+
+            int result;
+
+            // Prompt User for first input
+            Console.WriteLine("Enter a number");
+            int valueOne = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt User for the second input
+            Console.WriteLine("\nEnter another number, but you do not have to.");
+            string valueTwo = Console.ReadLine();
+
+            // Check to see if user input an empty value
+            if (string.IsNullOrEmpty(valueTwo))
+            {
+                result = add.Sum(valueOne);
+            }
+            else
+            {
+                result = add.Sum(valueOne, Convert.ToInt32(valueTwo));
+            }
+
+            // Display results
+            Console.WriteLine("\n" + result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
