0e77b8f6f576128eea45c3bb6d151f10a318bf6b
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-22 11:23:23 -0800

    initial commit

---
+++ Basic Assignments/ClassAndMethodsMath/ClassAndMethodsMath/MathClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsMath
+{
+    public class MathClass
+    {
+        // three math methods, each of which will take one integer parameter in and return an integer
+        public int sum(int num)
+        {
+            num = num + 33;
+            return num;
+        }
+
+        public int quotient(int num)
+        {
+            num = num / 2;
+            return num;
+        }
+
+        public int product(int num)
+        {
+            num = num * 3;
+            return num;
+        }
+
+        
+    }
+}
---
+++ Basic Assignments/ClassAndMethodsMath/ClassAndMethodsMath/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Prompt user for input
+                Console.WriteLine("Enter a number to perform basic math on... ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+
+                // Creating an object from the Class called MathClass
+                MathClass math = new MathClass();
+
+                // Calling the 3 math methods within the Class MathClass
+                // using the object math that was created prior
+                int sumResult = math.sum(num);
+                int quotientResult = math.quotient(num);
+                int productResult = math.product(num);
+
+
+                // Display results to User
+                Console.WriteLine("\n" + num + " + 33 = " + sumResult);
+                Console.WriteLine(num + " / 2 = " + quotientResult);
+                Console.WriteLine(num + " x 3 = " + productResult);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
