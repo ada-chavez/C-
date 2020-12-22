79285bb2d8249b52e69d46c4c09ec666dc12a4aa
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-22 12:04:47 -0800

    initial commit

---
+++ Basic Assignments/MethodOverload/MethodOverload/Mth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    public class Mth
+    {
+        //  Takes an integer and returns an integer
+        public int add (int num)
+        {
+            num = 2 + num;
+            return num;
+        }
+
+
+        // Takes a decimal and returns an integer
+        public int add (decimal num)
+        {
+            num = 3 + num;
+            return Convert.ToInt32(num);
+        }
+
+        // Takes a string and returns an integer
+        public int add (string num)
+        {
+            int number = Convert.ToInt32(num);
+
+            return 4 + number;
+
+        }
+
+    }
+}
---
+++ Basic Assignments/MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating Mth Class
+            Mth math = new Mth();
+
+            // Propmt User for input
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // Calling first add method
+            Console.WriteLine(math.add(num));
+
+            // Propmt User for input
+            Console.WriteLine("\nEnter a second number");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            // Calling second add method
+            Console.WriteLine(math.add(num2));
+
+            // Propmt User for input
+            Console.WriteLine("\nEnter a third number");
+            string num3 = Console.ReadLine();
+            // Calling third add method
+            Console.WriteLine(math.add(num3));
+
+            Console.ReadLine();
+
+        }
+    }
+}
