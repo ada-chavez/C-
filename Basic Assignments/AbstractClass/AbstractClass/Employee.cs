3ef637899f9f5490d402001ace9a4c0ddb2d4866
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-23 10:33:33 -0800

    initial commit

---
+++ Basic Assignments/AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // Inherit from Person Class
+   public class Employee : Person
+    {
+        // Methods
+        // Implement abstract SayName() method
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+    }
+}
---
+++ Basic Assignments/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Employee class
+            Employee employee = new Employee();
+
+            // Initialize Employee Object
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            // Call method SayName()
+            employee.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
