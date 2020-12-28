75efbba4b8de5eba12ddbc0660e1f980b27a9d02
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-28 09:29:17 -0800

    initial commit struct assignment

---
+++ Basic Assignments/StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // Create a Struct
+    public struct Number
+    {
+        // Property
+        public decimal Amount;
+    }
+}
---
+++ Basic Assignments/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare num1 of type Number
+            Number num1;
+
+            // Asign a number to Amount
+            num1.Amount = 268;
+
+            // Print Amount to console
+            Console.WriteLine("Amount: " + num1.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
