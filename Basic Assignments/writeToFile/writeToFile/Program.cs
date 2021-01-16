3326474ea752bb2befe3f969fa5364263b314e27
Ada Chavez <ada.r.chavez@gmail.com>
2021-01-16 10:48:07 -0800

    initial commit

---
+++ Basic Assignments/writeToFile/writeToFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace writeToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter a number:");
+            string userInput = Console.ReadLine();
+
+            // Log userInput number to text file
+            File.WriteAllText(@"C:\Users\Victoria\Logs\log.txt", userInput);
+
+            // Store text file into a string
+            string userNumber = File.ReadAllText(@"C:\Users\Victoria\Logs\log.txt");
+
+            // Print text file back to user
+            Console.WriteLine("\nHere is whats in the text file: " + userNumber );
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
